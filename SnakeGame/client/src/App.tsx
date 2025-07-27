a9ec11927a40002d1f62204a7c56538ba171c5a4
Abdullah.Ibrahim <126823435+Raven4x4@users.noreply.github.com>
2025-07-27 11:04:19 +0300

    client folder

---
+++ SnakeGame/client/src/App.tsx
@@ -0,0 +1,12 @@
+import MazeSolver from "./components/MazeSolver.tsx";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-gray-900 flex items-center justify-center">
+      <MazeSolver />
+    </div>
+  );
+}
+
+export default App;
